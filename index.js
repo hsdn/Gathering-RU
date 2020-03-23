ab2c99cffb458a2edb363001c0150e578b9c1bf0
Dmitry Shin <dmitry.s@hsdn.org>
2020-03-23 06:04:23 +0400

    Update

--- index.js
+++ index.js
@@ -1,100 +1,110 @@
 module.exports = function Gathering(mod) {
 	let plantsMarkers = false,
 		miningMarkers = false,
-		energyMarkers = false
-	
+		energyMarkers = false;
+
 	let mobid = [],
-		gatherMarker = []
-	
+		gatherMarker = [];
+
 	function gatheringStatus() {
 		sendStatus(
 			"Модуль: " + (mod.settings.enabled     ? "вкл"   : "выкл"),
 			"Предупреждающее сообщение: " + (mod.settings.sendToAlert  ? "вкл" : "выкл"),
-			
 			"Отображение растений: " + (plantsMarkers ? "вкл" : "выкл"),
 			"Отображение руды: " + (miningMarkers ? "вкл" : "выкл"),
 			"Отображение энергии: " + (energyMarkers ? "вкл" : "выкл")
-		)
+		);
 	}
-	
+
 	function sendStatus(msg) {
-		sendMessage([...arguments].join('\n\t - '))
+		sendMessage([...arguments].join('\n\t - '));
 	}
-	
+
 	mod.command.add("gat", (arg) => {
 		if (!arg) {
 			mod.settings.enabled = !mod.settings.enabled;
 			if (!mod.settings.enabled) {
-				plantsMarkers = false
-				miningMarkers = false
-				energyMarkers = false
+				plantsMarkers = false;
+				miningMarkers = false;
+				energyMarkers = false;
 				for (let itemId of mobid) {
-					despawnItem(itemId)
+					despawnItem(itemId);
 				}
 			}
-			gatheringStatus()
+			gatheringStatus();
 		} else {
 			switch (arg) {
 				case "alert":
-					mod.settings.sendToAlert = !mod.settings.sendToAlert
-					sendMessage("Предупреждающее сообщение " + (mod.settings.sendToAlert ? "вкл" : "выкл"))
+					mod.settings.sendToAlert = !mod.settings.sendToAlert;
+					sendMessage("Предупреждающее сообщение: " + (mod.settings.sendToAlert ? "вкл" : "выкл"));
 					break
 				case "status":
-					gatheringStatus()
+					gatheringStatus();
 					break
-				
 				case "plants":
-					plantsMarkers = !plantsMarkers
-					sendMessage("Отображение растений " + (plantsMarkers ? "вкл" : "выкл"))
+					plantsMarkers = !plantsMarkers;
+					sendMessage("Отображение растений: " + (plantsMarkers ? "вкл" : "выкл"));
 					break
 				case "ore":
-					miningMarkers = !miningMarkers
-					sendMessage("Отображение руды " + (miningMarkers ? "вкл" : "выкл"))
+					miningMarkers = !miningMarkers;
+					sendMessage("Отображение руды: " + (miningMarkers ? "вкл" : "выкл"));
 					break
 				case "energy":
-					energyMarkers = !energyMarkers
-					sendMessage("Отображение энергии " + (energyMarkers ? "вкл" : "выкл"))
+					energyMarkers = !energyMarkers;
+					sendMessage("Отображение энергии: " + (energyMarkers ? "вкл" : "выкл"));
 					break
-				
-				default :
-					sendMessage("Неверный параметр!")
+				default:
+					sendStatus(
+						"gat, включение модуля",
+						"gat alert, предупреждающее сообщение",
+						"gat status, состояние модуля",
+						"gat ore, включает отображение руды",
+						"gat plants, включает отображение растений",
+						"gat energy, включает отображение энергии"
+					);
 					break
 			}
 		}
 	})
-	
+
 	mod.game.me.on('change_zone', (zone, quick) => {
-		mobid = []
+		mobid = [];
 	})
-	
+
 	mod.hook('S_SPAWN_COLLECTION', 4, (event) => {
 		if (mod.settings.enabled) {
 			if (plantsMarkers && (gatherMarker = mod.settings.plants.find(obj => obj.id === event.id))) {
-				sendAlert( ("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
-				sendMessage("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg)
+				if (mod.settings.sendToAlert) {
+					sendAlert( ("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg), 44);
+					sendMessage("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg);
+				}
 			} else if (miningMarkers && (gatherMarker = mod.settings.mining.find(obj => obj.id === event.id))) {
-				sendAlert( ("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
-				sendMessage("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg)
+				if (mod.settings.sendToAlert) {
+					sendAlert( ("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg), 44);
+					sendMessage("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg);
+				}
 			} else if (energyMarkers && (gatherMarker = mod.settings.energy.find(obj => obj.id === event.id))) {
-				sendAlert( ("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg), 44)
-				sendMessage("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg)
+				if (mod.settings.sendToAlert) {
+					sendAlert( ("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg), 44);
+					sendMessage("Найдено [" + gatherMarker.name + "] " + gatherMarker.msg);
+				}
 			} else {
-				return true
+				return true;
 			}
-			
-			spawnItem(event.gameId, event.loc)
-			mobid.push(event.gameId)
+
+			spawnItem(event.gameId, event.loc);
+			mobid.push(event.gameId);
 		}
 	})
-	
+
 	mod.hook('S_DESPAWN_COLLECTION', 2, (event) => {
 		if (mobid.includes(event.gameId)) {
-			gatherMarker = []
-			despawnItem(event.gameId)
-			mobid.splice(mobid.indexOf(event.gameId), 1)
+			gatherMarker = [];
+			despawnItem(event.gameId);
+			mobid.splice(mobid.indexOf(event.gameId), 1);
 		}
 	})
-	
+
 	function spawnItem(gameId, loc) {
 		mod.send('S_SPAWN_DROPITEM', 8, {
 			gameId: gameId*10n,
@@ -102,13 +112,13 @@ module.exports = function Gathering(mod) {
 			item: mod.settings.markerId,
 			amount: 1,
 			expiry: 999999
-		})
+		});
 	}
-	
+
 	function despawnItem(gameId) {
 		mod.send('S_DESPAWN_DROPITEM', 4, {
 			gameId: gameId*10n
-		})
+		});
 	}
 	
 	function sendMessage(msg) { mod.command.message(msg) }
@@ -119,6 +129,6 @@ module.exports = function Gathering(mod) {
 			chat: false,
 			channel: 0,
 			message: msg,
-		})
-	}	
+		});
+	}
 }
